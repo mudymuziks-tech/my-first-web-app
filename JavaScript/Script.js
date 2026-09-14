e1e0fa361ccfb0a54b6d72b8b6ff3b27994fb4db
Mr.Mudy <mudymuziks@gmail.com>
2026-09-14 02:13:37 +0100

    add Explore back button

--- JavaScript/Script.js
+++ JavaScript/Script.js
@@ -1,15 +1,33 @@
 const searchInput = document.getElementById("movieSearch");
 const searchResult = document.getElementById("searchResult");
 
-searchInput.addEventListener("input", function () {
-  const searchText = searchInput.value;
+if (searchInput && searchResult) {
+  searchInput.addEventListener("input", function () {
+    const searchText = searchInput.value;
 
-  searchResult.textContent ="you are searching for: " + searchText;
-});
+    searchResult.textContent = "you are searching for: " + searchText;
+  });
+}
 
 
-const menuBtn = document .querySelector("#menuBtn");
-const menu = document .querySelector("#menu");
+const menuBtn = document.querySelector("#menuBtn");
+const menu = document.querySelector("#menu");
 
-menuBtn.addEventListener("click", () => {
-  menu.classList.toggle("show");});
+if (menuBtn && menu) {
+  menuBtn.addEventListener("click", () => {
+  menu.classList.toggle("show");
+  });
+}
+
+
+
+
+const backButton = document.querySelector("#backButton");
+
+console.log(backButton);
+
+if (backButton) {
+  backButton.addEventListener("click", () => {
+    history.back();
+  });
+}
